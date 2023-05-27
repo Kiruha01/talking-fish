6153563d00475d7a7b19eb51fab5c5385b1bae94
Kiruha01 <liss-2001g@mail.ru>
2023-05-27 22:11:57 +0300

    connect fish body

--- MotionController.h
+++ MotionController.h
@@ -2,18 +2,120 @@
 
 #define BUFFER_LEN 1000
 
+
 class MotionController{
   private:
     DynamicJsonDocument* jsonDoc;
+    
+    int head[100];
+    int currentHeadIndex = 0;
+    int headLen = 0;
+    bool headState = false;
+    
+    int tail[100];
+    int tailLen = 0;
+    int currentTailIndex = 0;
+    bool tailState = false;
+    
+    int mouth[500];
+    int mouthLen = 0;
+    int currentMouthIndex = 0;
+    bool mouthState = false;
+
+    unsigned long startTime;
+
+    void toogleHead(){
+      #ifdef DEBUG_FLAG
+      Serial.print("HEAD STATE: ");
+      Serial.println(!headState);
+      #endif
+      headState = !headState;
+      digitalWrite(HEAD_PIN, headState);
+      digitalWrite(TAIL_PIN, LOW);
+    }
+    void toogleTail(){
+      #ifdef DEBUG_FLAG
+      Serial.print("TAIL STATE: ");
+      Serial.println(!tailState);
+      #endif
+      tailState = !tailState;
+      digitalWrite(TAIL_PIN, tailState);
+      digitalWrite(HEAD_PIN, LOW);
+    }
+    void toogleMouth(){
+      #ifdef DEBUG_FLAG
+      Serial.print("MOUTH STATE: ");
+      Serial.println(!mouthState);
+      #endif
+      mouthState = !mouthState;
+      digitalWrite(MOUTH_PIN, mouthState);
+    }
 
   public:
-    void setSmt(String cc){
-      jsonDoc = new DynamicJsonDocument(900);
+    void setMotion(String cc){
+      jsonDoc = new DynamicJsonDocument(BUFFER_LEN); // TODO: on json size
       DeserializationError err = deserializeJson(*jsonDoc, cc);
       if (err != DeserializationError::Ok){
         Serial.println("Error");
+        Serial.println(err.f_str());
       }
-      JsonArray head = (*jsonDoc)["h"];
-      Serial.println(head[0].as<int>());
+        if ((*jsonDoc)["h"].is<JsonArray>()) {
+          JsonArray s = (*jsonDoc)["h"];
+          headLen = s.size();
+          for (int i = 0; i<headLen; ++i){
+            Serial.print(s[i].as<int>());
+            Serial.print(" ");
+            head[i] = s[i].as<int>();
+          }
+        }
+       Serial.println();
+        if ((*jsonDoc)["t"].is<JsonArray>()) {
+          JsonArray s = (*jsonDoc)["t"];
+          tailLen = s.size();
+          for (int i = 0; i<tailLen; ++i){
+            Serial.print(s[i].as<int>());
+            Serial.print(" ");
+            tail[i] = s[i].as<int>();
+          }
+        }
+       Serial.println();
+       if ((*jsonDoc)["m"].is<JsonArray>()) {
+          JsonArray s = (*jsonDoc)["m"];
+          mouthLen = s.size();
+          for (int i = 0; i<mouthLen; ++i){
+            Serial.print(s[i].as<int>());
+            Serial.print(" ");
+            mouth[i] = s[i].as<int>();
+          }
+        }
+       Serial.println();
+
     }
+
+    void start(){
+      startTime = millis();
+      currentHeadIndex = 0;
+      currentTailIndex = 0;
+      currentMouthIndex = 0;
+    }
+
+    void loopTick(){
+
+      unsigned long delayTime = millis() - startTime;
+      if (delayTime >= head[currentHeadIndex] and currentHeadIndex < headLen){
+        toogleHead();
+          ++currentHeadIndex;
+      }
+      if (delayTime >= tail[currentTailIndex] and currentTailIndex < tailLen){
+        toogleTail();
+        ++currentTailIndex;
+      }
+      if (delayTime >= mouth[currentMouthIndex] and currentMouthIndex < mouthLen){
+        toogleMouth();
+        ++currentMouthIndex;
+      }
+
+    }
+
+    
 };
---
+++ talking_fish.ino
@@ -0,0 +1,111 @@
+#include "Arduino.h"
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266HTTPClient.h>
+
+// ========= CONFIG =========
+#include "config.h"
+#define DEBUG_FLAG
+
+#ifndef STASSID
+#define STASSID "ssid"
+#define STAPSK  "password"
+#endif
+
+#ifndef HEAD_PIN
+#define HEAD_PIN D6
+#define TAIL_PIN D7
+#define MOUTH_PIN D8
+#endif
+
+#include "MusicPlayer.h"
+#include "MotionController.h"
+
+// ==========================
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+MusicPlayer player;
+MotionController motion;
+
+/* ################################## Setup ############################################# */
+String URL = "http://192.168.0.101:8080/data";
+
+void setup() {
+  pinMode(HEAD_PIN, OUTPUT);
+  pinMode(TAIL_PIN, OUTPUT);
+  pinMode(MOUTH_PIN, OUTPUT);
+#ifdef DEBUG_FLAG
+  Serial.begin(115200);
+#endif
+  delay(1000);
+#ifdef DEBUG_FLAG
+  Serial.println("Connecting to WiFi");
+#endif
+
+  WiFi.disconnect();
+  WiFi.softAPdisconnect(true);
+  WiFi.mode(WIFI_STA);
+
+  WiFi.begin(ssid, password);
+
+  // Try forever
+  while (WiFi.status() != WL_CONNECTED) {
+#ifdef DEBUG_FLAG
+    Serial.println("...Connecting to WiFi");
+#endif
+
+    delay(1000);
+  }
+#ifdef DEBUG_FLAG
+  Serial.println("Connected");
+#endif
+
+  motion.setMotion(getJsonData(URL + ".json"));
+  motion.start();
+  player.play(URL + ".mp3");
+
+}
+
+/* #################################### Loop ############################################## */
+
+void loop() {
+  player.loopTick();
+  motion.loopTick();
+}
+
+/* ################################## Network ############################################# */
+
+String getJsonData(String url) {
+  WiFiClient client;
+
+  HTTPClient http;
+  if (http.begin(client, url)) {
+    int httpCode = http.GET();
+    if (httpCode > 0) {
+#ifdef DEBUG_FLAG
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+#endif
+
+
+      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+        String payload = http.getString();
+#ifdef DEBUG_FLAG
+        Serial.println(payload);
+#endif
+        http.end();
+        return payload;
+      }
+    } else {
+      Serial.println(url);
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+  } else {
+    Serial.printf("[HTTP} Unable to connect\n");
+  }
+  return "";
+
+}
